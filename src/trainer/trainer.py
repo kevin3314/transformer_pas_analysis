ec6f7ceef0007d799b8f54cc085e383f90dfc27b
nobu-g <ueda-n127@hotmail.co.jp>
2019-04-24 18:39:21 +0900

    make it work

--- src/trainer/trainer.py
+++ src/trainer/trainer.py
@@ -48,28 +48,28 @@ class Trainer(BaseTrainer):
 
         total_loss = 0
         total_metrics = np.zeros(len(self.metrics))
-        for batch_idx, (data, target) in enumerate(self.data_loader):
-            data, target = data.to(self.device), target.to(self.device)
-
-            self.optimizer.zero_grad()
-            output = self.model(data)
-            loss = self.loss(output, target)
-            loss.backward()
-            self.optimizer.step()
-
-            self.writer.set_step((epoch - 1) * len(self.data_loader) + batch_idx)
-            self.writer.add_scalar('loss', loss.item())
-            total_loss += loss.item()
-            total_metrics += self._eval_metrics(output, target)
-
-            if self.verbosity >= 2 and batch_idx % self.log_step == 0:
-                self.logger.info('Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}'.format(
-                    epoch,
-                    batch_idx * self.data_loader.batch_size,
-                    self.data_loader.n_samples,
-                    100.0 * batch_idx / len(self.data_loader),
-                    loss.item()))
-                self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
+        # for batch_idx, (data, target) in enumerate(self.data_loader):
+        #     data, target = data.to(self.device), target.to(self.device)
+        #
+        #     self.optimizer.zero_grad()
+        #     output = self.model(data)
+        #     loss = self.loss(output, target)
+        #     loss.backward()
+        #     self.optimizer.step()
+        #
+        #     self.writer.set_step((epoch - 1) * len(self.data_loader) + batch_idx)
+        #     self.writer.add_scalar('loss', loss.item())
+        #     total_loss += loss.item()
+        #     total_metrics += self._eval_metrics(output, target)
+        #
+        #     if self.verbosity >= 2 and batch_idx % self.log_step == 0:
+        #         self.logger.info('Train Epoch: {} [{}/{} ({:.0f}%)] Loss: {:.6f}'.format(
+        #             epoch,
+        #             batch_idx * self.data_loader.batch_size,
+        #             self.data_loader.n_samples,
+        #             100.0 * batch_idx / len(self.data_loader),
+        #             loss.item()))
+        #         self.writer.add_image('input', make_grid(data.cpu(), nrow=8, normalize=True))
 
         log = {
             'loss': total_loss / len(self.data_loader),
