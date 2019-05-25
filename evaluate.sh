2c5b69adef200afaac35e55ea5d67c8e13c34414
nobu-g <ueda-n127@hotmail.co.jp>
2019-05-25 20:02:46 +0900

    fix evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -10,10 +10,11 @@ OUTPUT_BASE_DIR=$1  # the first argument
 TARGET=$2  # the second argument
 OUT_KNP_DIR=${OUTPUT_BASE_DIR}/${TARGET}_out_knp
 RESULT_FILE=${OUTPUT_BASE_DIR}/result.txt
+RESULT_HTML_FILE=${OUTPUT_BASE_DIR}/result.html
 
 # convert conll file to knp file
 PYTHONPATH=${NN_BASED_ANAPHORA_RESOLUTION_DIR}/scripts python ${NN_BASED_ANAPHORA_RESOLUTION_DIR}/scripts/corpus/conll2knp.py --output_dir ${OUT_KNP_DIR} < ${OUTPUT_BASE_DIR}/${TARGET}_out.conll
 # evaluate knp file
 ${PYTHON2_COMMAND} ${NN_BASED_ANAPHORA_RESOLUTION_DIR}/scripts/scorer.py --knp_dir ${EVAL_CORPUS_BASEDIR}/knp_add_feature --dev_id_file ${EVAL_CORPUS_BASEDIR}/dev.files --test_id_file ${EVAL_CORPUS_BASEDIR}/test.files --system_dir ${OUT_KNP_DIR} --target ${TARGET} --inter_sentential --relax_evaluation --not_fix_case_analysis --relax_evaluation_multiple_argument > ${RESULT_FILE} 2> ${RESULT_FILE}.log
 # convert result file to html format
-$(PYTHON2_COMMAND) $(NN_BASED_ANAPHORA_RESOLUTION_DIR)/scripts/bert_result2html.py --result_file $< > $@
+${PYTHON2_COMMAND} ${NN_BASED_ANAPHORA_RESOLUTION_DIR}/scripts/bert_result2html.py --result_file ${RESULT_FILE} > ${RESULT_HTML_FILE}
