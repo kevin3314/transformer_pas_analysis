17ca14fadab1bd7447a6a3d0eb07c29c9c5a676f
Nobuhiro Ueda <ueda@nlp.ist.i.kyoto-u.ac.jp>
2019-10-28 01:19:37 +0900

    revise log level

--- src/kwdlc_reader/base_phrase.py
+++ src/kwdlc_reader/base_phrase.py
@@ -36,7 +36,7 @@ class BasePhrase:
             if '<内容語>' in mrph.fstring:
                 return mrph2dmid[mrph]
         else:
-            logger.warning(f'{self.sid:21}cannot find content word in: {self.tag.midasi}')
+            logger.info(f'{self.sid:21}cannot find content word in: {self.tag.midasi}. Use first mrph instead')
             return mrph2dmid[self.tag.mrph_list()[0]]
     
     @property
