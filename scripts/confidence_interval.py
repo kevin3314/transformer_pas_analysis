bfe00f81c860fe8dd512dc4a329932a565b32343
Nobuhiro Ueda <ueda@nlp.ist.i.kyoto-u.ac.jp>
2020-01-02 21:24:15 +0900

    modify confidence_interval.py

--- scripts/confidence_interval.py
+++ scripts/confidence_interval.py
@@ -10,6 +10,9 @@ def main():
 
     columns = []
     uppers = []
+    deltas_plus = []
+    middles = []
+    deltas_minus = []
     lowers = []
     for column, items in df.iteritems():
         columns.append(column)
@@ -20,43 +23,25 @@ def main():
         loc: 平均 X bar
         scale: 標準偏差 s
         """
-        interval = stats.t.interval(alpha=0.95,
-                                    df=n - 1,
-                                    loc=items.mean(),
-                                    scale=math.sqrt(items.var() / n))
-        uppers.append(interval[0])
-        lowers.append(interval[1])
+        upper, lower = stats.t.interval(alpha=0.95,
+                                        df=n - 1,
+                                        loc=items.mean(),
+                                        scale=math.sqrt(items.var() / n))
+        middle = (upper + lower) / 2
+        uppers.append(upper)
+        deltas_plus.append(upper - middle)
+        middles.append(middle)
+        deltas_minus.append(lower - middle)
+        lowers.append(lower)
 
-    df_interval = pd.DataFrame.from_dict({'upper': uppers, 'lower': lowers},
+    df_interval = pd.DataFrame.from_dict({'upper': uppers,
+                                          'delta+': deltas_plus,
+                                          'middle': middles,
+                                          'delta-': deltas_minus,
+                                          'lower': lowers},
                                          orient='index',
                                          columns=columns)
     print(df_interval)
-
-
-def sample():
-    iris = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data',
-                       header=None, names=['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class'])
-
-    setosa = iris[iris['class'] == 'Iris-setosa']
-    virginica = iris[iris['class'] == 'Iris-virginica']
-    versicolor = iris[iris['class'] == 'Iris-versicolor']
-
-    print(setosa['sepal_width'].mean())
-
-    print(setosa['sepal_width'].var())
-
-    n = 300
-    """
-    alpha: 何パーセント信頼区間か
-    df: t分布の自由度
-    loc: 平均 X bar
-    scale: 標準偏差 s
-    """
-    interval = stats.t.interval(alpha=0.95,
-                                df=n - 1,
-                                loc=setosa.iloc[:n]['sepal_width'].mean(),
-                                scale=math.sqrt(setosa.iloc[:n]['sepal_width'].var() / n))
-    print(interval)
 
 
 if __name__ == '__main__':
