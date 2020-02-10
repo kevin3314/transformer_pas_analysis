34676fb58b55c8511d168e61c1a4a90424f08689
Nobuhiro Ueda <ueda@nlp.ist.i.kyoto-u.ac.jp>
2020-02-10 19:09:57 +0900

    update packages

--- src/data_loader/data_loaders.py
+++ src/data_loader/data_loaders.py
@@ -2,6 +2,6 @@ from base import BaseDataLoader
 from data_loader.dataset.dataset import PASDataset
 
 
-class ConllDataLoader(BaseDataLoader):
+class PASDataLoader(BaseDataLoader):
     def __init__(self, dataset: PASDataset, batch_size: int, shuffle: bool, validation_split: float, num_workers: int):
         super().__init__(dataset, batch_size, shuffle, validation_split, num_workers)
