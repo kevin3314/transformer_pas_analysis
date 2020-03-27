da7c8a093db24e080eb0fe30ee3dd44c6525fc3a
Nobuhiro Ueda <ueda@nlp.ist.i.kyoto-u.ac.jp>
2020-03-27 16:54:04 +0900

    add RefinementModel2

--- src/model/model.py
+++ src/model/model.py
@@ -253,6 +253,43 @@ class RefinementModel(BaseModel):
         return base_logits, base_logits.detach() + modification  # (b, seq, case, seq), (b, seq, case, seq)
 
 
+class RefinementModel2(BaseModel):
+    """最終結果に前段の出力を使わない"""
+    def __init__(self,
+                 bert_model: str,
+                 vocab_size: int,
+                 dropout: float,
+                 num_case: int,
+                 coreference: bool,
+                 refinement_type: int,  # 1 or 2 or 3
+                 refinement_bert_model: str,
+                 ) -> None:
+        super().__init__()
+
+        self.baseline_model = BaselineModel(bert_model, vocab_size, dropout, num_case, coreference)
+        args = (refinement_bert_model, vocab_size, dropout, num_case, coreference)
+        if refinement_type == 1:
+            self.refinement_layer = RefinementLayer1(*args)
+        elif refinement_type == 2:
+            self.refinement_layer = RefinementLayer2(*args)
+        elif refinement_type == 3:
+            self.refinement_layer = RefinementLayer3(*args)
+        self.mask = Mask()
+
+    def forward(self,
+                input_ids: torch.Tensor,       # (b, seq)
+                attention_mask: torch.Tensor,  # (b, seq)
+                segment_ids: torch.Tensor,     # (b, seq)
+                ng_token_mask: torch.Tensor,   # (b, seq, case, seq)
+                deps: torch.Tensor,            # (b, seq, seq)
+                ) -> Tuple[torch.Tensor, torch.Tensor]:  # (b, seq, case, seq), (b, seq, case, seq)
+        # (b, seq, case, seq)
+        base_logits = self.baseline_model(input_ids, attention_mask, segment_ids, ng_token_mask, deps)
+        refined = self.refinement_layer(input_ids, attention_mask, base_logits.detach().softmax(dim=3))
+
+        return base_logits, self.mask(refined)  # (b, seq, case, seq), (b, seq, case, seq)
+
+
 class DuplicateModel(BaseModel):
     """RefinementModel の前段の logits を後段に与えないモデル"""
 
