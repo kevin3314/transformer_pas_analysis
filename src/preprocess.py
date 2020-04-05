142ab76f91c3464d5eee2c8692f2823e7fedd3dc
Nobuhiro Ueda <ueda@nlp.ist.i.kyoto-u.ac.jp>
2020-04-05 20:47:03 +0900

    add kc splitter to preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,18 +1,87 @@
 """Preprocess dataset."""
 import argparse
+import json
 from pathlib import Path
 import _pickle as cPickle
-import json
-from typing import List
+from typing import List, Dict
+from collections import defaultdict
 
+from pyknp import BList
 from kyoto_reader import KyotoReader
+from transformers import BertTokenizer
 
 from data_loader.dataset.commonsense_dataset import CommonsenseExample
 
+MAX_SUBWORD_LEN = 121
 
-def process(input_path: Path, output_path: Path, cases: List[str], corefs: List[str]):
+
+def process_kwdlc(input_path: Path, output_path: Path, cases: List[str], corefs: List[str]):
     output_path.mkdir(exist_ok=True)
     reader = KyotoReader(input_path, cases, corefs, extract_nes=False)
+    documents = reader.process_all_documents()
+    for document in documents:
+        with output_path.joinpath(document.doc_id + '.pkl').open(mode='wb') as f:
+            cPickle.dump(document, f)
+
+
+def split_kc(input_dir: Path, output_dir: Path, tokenizer: BertTokenizer):
+    """
+    各文書を，tokenize したあとの長さが MAX_SUBWORD_LEN 以下になるように複数の文書に分割する．
+    1文に分割しても MAX_SUBWORD_LEN を超えるような，長い文はそのまま出力する
+    """
+    did2sids: Dict[str, List[str]] = defaultdict(list)
+    did2cumlens: Dict[str, List[int]] = {}
+    sid2knp: Dict[str, str] = {}
+
+    print(f'reading from {input_dir}...')
+    for knp_file in input_dir.glob('*.knp'):
+        with knp_file.open() as fin:
+            did = knp_file.stem
+            did2cumlens[did] = [0]
+            buff = ''
+            for line in fin:
+                buff += line
+                if line.strip() == 'EOS':
+                    blist = BList(buff)
+                    did2sids[did].append(blist.sid)
+                    did2cumlens[did].append(
+                        did2cumlens[did][-1] + len(tokenizer.tokenize(' '.join(m.midasi for m in blist.mrph_list())))
+                    )
+                    sid2knp[blist.sid] = buff
+                    buff = ''
+
+    print(f'writing to {output_dir}...')
+    for did, sids in did2sids.items():
+        cum: List[int] = did2cumlens[did]
+        end = 1
+        # end を探索
+        while cum[end+1] - cum[0] <= MAX_SUBWORD_LEN:
+            end += 1
+            if end >= len(sids):
+                break
+
+        idx = 0
+        while end < len(sids) + 1:
+            start = 0
+            # start を探索
+            while cum[end] - cum[start] > MAX_SUBWORD_LEN:
+                start += 1
+                if start == end - 1:
+                    break
+            with output_dir.joinpath(f'{did}-{idx:02}.knp').open('wt') as fout:
+                fout.write(''.join(sid2knp[sid] for sid in sids[start:end]))  # start から end まで書き出し
+            idx += 1
+            end += 1
+
+
+def process_kc(input_path: Path, output_path: Path, cases: List[str], corefs: List[str], tokenizer: BertTokenizer):
+    tmp_dir = Path('/tmp/kc')
+    tmp_dir.mkdir(exist_ok=True)
+    # 京大コーパスは1文書が長いのでできるだけ多くの context を含むように複数文書に分割する
+    split_kc(input_path, tmp_dir, tokenizer)
+
+    output_path.mkdir(exist_ok=True)
+    reader = KyotoReader(tmp_dir, cases, corefs, extract_nes=False)
     documents = reader.process_all_documents()
     for document in documents:
         with output_path.joinpath(document.doc_id + '.pkl').open(mode='wb') as f:
@@ -44,6 +113,8 @@ def main():
                         help='Coreference strings. Separate by ","')
     parser.add_argument('--case-string', type=str, default='ガ,ヲ,ニ,ガ２,ノ,ノ？,判ガ',
                         help='Case strings. Separate by ","')
+    parser.add_argument('--bert-model', type=str, default=None,
+                        help='path to pre-trained BERT model directory')
     args = parser.parse_args()
 
     # make directories to save dataset
@@ -61,17 +132,18 @@ def main():
         kwdlc_dir = Path(args.kwdlc).resolve()
         output_dir = Path(args.out) / 'kwdlc'
         output_dir.mkdir(exist_ok=True)
-        process(kwdlc_dir / 'train', output_dir / 'train', target_cases, target_corefs)
-        process(kwdlc_dir / 'valid', output_dir / 'valid', target_cases, target_corefs)
-        process(kwdlc_dir / 'test', output_dir / 'test', target_cases, target_corefs)
+        process_kwdlc(kwdlc_dir / 'train', output_dir / 'train', target_cases, target_corefs)
+        process_kwdlc(kwdlc_dir / 'valid', output_dir / 'valid', target_cases, target_corefs)
+        process_kwdlc(kwdlc_dir / 'test', output_dir / 'test', target_cases, target_corefs)
 
     if args.kc is not None:
         kc_dir = Path(args.kc).resolve()
         output_dir = Path(args.out) / 'kc'
         output_dir.mkdir(exist_ok=True)
-        process(kc_dir / 'train', output_dir / 'train', target_cases, target_corefs)
-        process(kc_dir / 'valid', output_dir / 'valid', target_cases, target_corefs)
-        process(kc_dir / 'test', output_dir / 'test', target_cases, target_corefs)
+        tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case=False, tokenize_chinese_chars=False)
+        process_kc(kc_dir / 'train', output_dir / 'train', target_cases, target_corefs, tokenizer)
+        process_kc(kc_dir / 'valid', output_dir / 'valid', target_cases, target_corefs, tokenizer)
+        process_kc(kc_dir / 'test', output_dir / 'test', target_cases, target_corefs, tokenizer)
 
     if args.commonsense is not None:
         commonsense_dir = Path(args.commonsense).resolve()
