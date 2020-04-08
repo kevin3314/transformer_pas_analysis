e7c91f100cab43af3893b81929aedd0d9efd8505
Nobuhiro Ueda <ueda@nlp.ist.i.kyoto-u.ac.jp>
2020-04-08 15:19:11 +0900

    tweak

--- src/preprocess.py
+++ src/preprocess.py
@@ -33,7 +33,7 @@ def split_kc(input_dir: Path, output_dir: Path, tokenizer: BertTokenizer):
     did2cumlens: Dict[str, List[int]] = {}
     sid2knp: Dict[str, str] = {}
 
-    print(f'reading from {input_dir}...')
+    # print(f'reading from {input_dir}...')
     for knp_file in input_dir.glob('*.knp'):
         with knp_file.open() as fin:
             did = knp_file.stem
@@ -50,7 +50,7 @@ def split_kc(input_dir: Path, output_dir: Path, tokenizer: BertTokenizer):
                     sid2knp[blist.sid] = buff
                     buff = ''
 
-    print(f'writing to {output_dir}...')
+    # print(f'writing to {output_dir}...')
     for did, sids in did2sids.items():
         cum: List[int] = did2cumlens[did]
         end = 1
@@ -132,6 +132,7 @@ def main():
         kwdlc_dir = Path(args.kwdlc).resolve()
         output_dir = Path(args.out) / 'kwdlc'
         output_dir.mkdir(exist_ok=True)
+        print('processing kwdlc...')
         process_kwdlc(kwdlc_dir / 'train', output_dir / 'train', target_cases, target_corefs)
         process_kwdlc(kwdlc_dir / 'valid', output_dir / 'valid', target_cases, target_corefs)
         process_kwdlc(kwdlc_dir / 'test', output_dir / 'test', target_cases, target_corefs)
@@ -141,6 +142,7 @@ def main():
         output_dir = Path(args.out) / 'kc'
         output_dir.mkdir(exist_ok=True)
         tokenizer = BertTokenizer.from_pretrained(args.bert_model, do_lower_case=False, tokenize_chinese_chars=False)
+        print('processing kc...')
         process_kc(kc_dir / 'train', output_dir / 'train', target_cases, target_corefs, tokenizer)
         process_kc(kc_dir / 'valid', output_dir / 'valid', target_cases, target_corefs, tokenizer)
         process_kc(kc_dir / 'test', output_dir / 'test', target_cases, target_corefs, tokenizer)
@@ -149,6 +151,7 @@ def main():
         commonsense_dir = Path(args.commonsense).resolve()
         output_dir = Path(args.out) / 'commonsense'
         output_dir.mkdir(exist_ok=True)
+        print('processing commonsense...')
         process_commonsense(commonsense_dir / 'train.csv', output_dir / 'train.pkl')
         process_commonsense(commonsense_dir / 'valid.csv', output_dir / 'valid.pkl')
         process_commonsense(commonsense_dir / 'test.csv', output_dir / 'test.pkl')
