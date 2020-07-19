e468c8de7da2063eeee5643e25411f2b53868f46
Nobuhiro Ueda <ueda@nlp.ist.i.kyoto-u.ac.jp>
2020-07-19 21:16:35 +0900

    fix servers

--- src/api_server.py
+++ src/api_server.py
@@ -7,9 +7,10 @@ import argparse
 from flask import Flask, request, jsonify, make_response
 from kyoto_reader import Document
 
-from writer.prediction_writer import PredictionKNPWriter
+from prediction.prediction_writer import PredictionKNPWriter
 from analyzer import Analyzer
-from inference import draw_tree
+from predict import draw_tree
+from utils.parse_config import ConfigParser
 
 app = Flask(__name__)
 
@@ -63,8 +64,14 @@ def api():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', '-r', '--resume', required=True, type=str,
+    parser.add_argument('-r', '--resume', '-m', '--model', default=None, type=str,
                         help='path to trained checkpoint')
+    parser.add_argument('--ens', '--ensemble', default=None, type=str,
+                        help='path to directory where checkpoints to ensemble exist')
+    parser.add_argument('-d', '--device', default='', type=str,
+                        help='indices of GPUs to enable (default: all)')
+    parser.add_argument('-c', '--config', default=None, type=str,
+                        help='config file path (default: None)')
     parser.add_argument('--host', default='0.0.0.0', type=str,
                         help='host ip address (default: 0.0.0.0)')
     parser.add_argument('--port', default=12345, type=int,
@@ -72,7 +79,7 @@ if __name__ == '__main__':
     parser.add_argument('--use-bertknp', action='store_true', default=False,
                         help='use BERTKNP in base phrase segmentation and parsing')
     args = parser.parse_args()
-
-    analyzer = Analyzer(args.model, device='', logger=logger, bertknp=args.use_bertknp)
+    config = ConfigParser.from_parser(parser, run_id='')
+    analyzer = Analyzer(config, logger=logger, bertknp=args.use_bertknp)
 
     app.run(host=args.host, port=args.port, debug=False, threaded=False)
--- src/xmlrpc_server.py
+++ src/xmlrpc_server.py
@@ -1,19 +1,13 @@
-import os
 import io
-import sys
 import logging
 import argparse
 from xmlrpc.server import SimpleXMLRPCServer
 
-from writer.prediction_writer import PredictionKNPWriter
+from prediction.prediction_writer import PredictionKNPWriter
 from analyzer import Analyzer
+from utils.parse_config import ConfigParser
 
 logger = logging.getLogger(__name__)
-
-
-def show_usage():
-    print('Usage:')
-    print('$ env HOST=<HOST> PORT=<PORT> poetry run server.py')
 
 
 def analyze_raw_data_from_client(knp_result: str):
@@ -27,19 +21,25 @@ def analyze_raw_data_from_client(knp_result: str):
 
 
 if __name__ == '__main__':
-    HOST = os.environ.get('HOST')
-    PORT = os.environ.get('PORT')
-    if not all((HOST, PORT)):
-        show_usage()
-        sys.exit()
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', required=True, type=str,
+    parser.add_argument('-r', '--resume', '-m', '--model', default=None, type=str,
                         help='path to trained checkpoint')
+    parser.add_argument('--ens', '--ensemble', default=None, type=str,
+                        help='path to directory where checkpoints to ensemble exist')
+    parser.add_argument('-d', '--device', default='', type=str,
+                        help='indices of GPUs to enable (default: all)')
+    parser.add_argument('-c', '--config', default=None, type=str,
+                        help='config file path (default: None)')
+    parser.add_argument('--host', default='0.0.0.0', type=str,
+                        help='host ip address (default: 0.0.0.0)')
+    parser.add_argument('--port', default=12345, type=int,
+                        help='host port number (default: 12345)')
+    parser.add_argument('--use-bertknp', action='store_true', default=False,
+                        help='use BERTKNP in base phrase segmentation and parsing')
     args = parser.parse_args()
+    config = ConfigParser.from_parser(parser, run_id='')
+    analyzer = Analyzer(config, logger=logger, bertknp=args.use_bertknp)
 
-    analyzer = Analyzer(args.model, device='', logger=logger, bertknp=True)
-
-    server = SimpleXMLRPCServer((HOST, int(PORT)))
+    server = SimpleXMLRPCServer((args.host, args.port))
     server.register_function(analyze_raw_data_from_client)
     server.serve_forever()
