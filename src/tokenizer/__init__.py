1e40008bbf86d0393a28f1ed4360179dd0ad196c
Akutagawa <pbwfh384@gmail.com>
2021-02-02 00:16:37 +0900

    Add bibart tokenizer

--- src/tokenizer/__init__.py
+++ src/tokenizer/__init__.py
@@ -1,5 +1,6 @@
 from .bart_spm_tokenizer import BartSPMTokenizeHandler
 from .bert_tokenizer import BertTokenizeHandler
+from .bibart_tokenizer import BiBartTokenizeHandler
 from .t5_tokenizer import T5TokenizeHandler
 from .mbart_tokenizer import MBartTokenizerHandler
 from .utils import TokenizeHandlerMeta
