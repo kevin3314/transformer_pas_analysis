64ecb8be2ab7a35fd24b01200cfa51cda6bf571a
Akutagawa <pbwfh384@gmail.com>
2021-02-01 16:51:05 +0900

    Add special model

--- src/model/model.py
+++ src/model/model.py
@@ -127,6 +127,67 @@ class BaselineMBartModel(nn.Module):
         return loss, output
 
 
+class SpecialEmbInitModel(nn.Module):
+    """Special token の埋め込み表現をうまく初期化"""
+
+    def __init__(self,
+                 pretrained_path: str,
+                 special_emb_path: str,
+                 vocab_size: int,
+                 dropout: float,
+                 num_case: int,
+                 coreference: bool,
+                 **_
+                 ) -> None:
+        super().__init__()
+
+        self.bert: BertModel = BertModel.from_pretrained(pretrained_path)
+        self.bert.resize_token_embeddings(vocab_size)
+        self.dropout = nn.Dropout(dropout)
+
+        special_embs = torch.load(special_emb_path)
+        self.bert.set_input_embeddings(nn.Embedding.from_pretrained(special_embs, padding_idx=0))
+
+        # emb = self.bert.get_input_embeddings()
+        # emb_weight = emb.weight
+        # emb_weight[orig_vocab_size:, :] = special_embs
+        # self.bert.get_input_embeddings().weight = nn 
+        # emb.requires_grad = True
+
+        self.num_case = num_case + int(coreference)
+        bert_hidden_size = self.bert.config.hidden_size
+
+        self.l_prd = nn.Linear(bert_hidden_size, bert_hidden_size * self.num_case)
+        self.l_arg = nn.Linear(bert_hidden_size, bert_hidden_size * self.num_case)
+        self.out = nn.Linear(bert_hidden_size, 1, bias=False)
+
+    def forward(self,
+                input_ids: torch.Tensor,       # (b, seq)
+                attention_mask: torch.Tensor,  # (b, seq)
+                segment_ids: torch.Tensor,     # (b, seq)
+                ng_token_mask: torch.Tensor,   # (b, seq, case, seq)
+                target: torch.Tensor,          # (b, seq, case, seq)
+                **_
+                ) -> Tuple[torch.Tensor, ...]:  # (), (b, seq, case, seq)
+        batch_size, sequence_len = input_ids.size()
+        mask = get_mask(attention_mask, ng_token_mask)
+        # (b, seq, hid)
+        sequence_output, _ = self.bert(input_ids, attention_mask=attention_mask, token_type_ids=segment_ids)
+
+        h_p = self.l_prd(self.dropout(sequence_output))  # (b, seq, case*hid)
+        h_a = self.l_arg(self.dropout(sequence_output))  # (b, seq, case*hid)
+        h_p = h_p.view(batch_size, sequence_len, self.num_case, -1)  # (b, seq, case, hid)
+        h_a = h_a.view(batch_size, sequence_len, self.num_case, -1)  # (b, seq, case, hid)
+        h_pa = torch.tanh(self.dropout(h_p.unsqueeze(2) + h_a.unsqueeze(1)))  # (b, seq, seq, case, hid)
+        # -> (b, seq, seq, case, 1) -> (b, seq, seq, case) -> (b, seq, case, seq)
+        output = self.out(h_pa).squeeze(-1).transpose(2, 3).contiguous()
+        output += (~mask).float() * -1024.0
+
+        loss = cross_entropy_pas_loss(output, target)
+
+        return loss, output
+
+
 class RefinementModel(nn.Module):
     def __init__(self,
                  pretrained_path: str,
