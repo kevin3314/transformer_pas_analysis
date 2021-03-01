002f04e4925011bddbe33747d82e1c4d8263509a
Akutagawa <pbwfh384@gmail.com>
2021-03-01 20:54:58 +0900

    Update for new class

--- src/data_loader/dataset/__init__.py
+++ src/data_loader/dataset/__init__.py
@@ -1,3 +1,3 @@
-from .read_example import PasExample
-from .pas_dataset import InputFeatures, PASDataset
+from .read_example import PasExample, PasExampleForEncDec
+from .pas_dataset import InputFeatures, PASDataset, InputFeaturesForEncDec, PASDatasetForEncDec
 from .commonsense_dataset import CommonsenseDataset
